c51f9683ba7f6148174113a030d3d38c2c7fa6ea
kiwei <jiaweiqng.work@qq.com>
2017-02-17 16:31:10 +0800

    [vsync.sh]Modify vsync.sh For use.
    修改vsync.sh，方便后续拓展使用.

--- vsync.sh
+++ vsync.sh
@@ -1,9 +1,8 @@
-#!/bin/sh
-#此脚本用于远程copy文件或者文件夹之用
 
-REMOTE_USER=wuhan
-REMOTE_IP=183.62.205.132
-REMOTE_DIR_BASE=/home/${REMOTE_USER}/jiawq/
+read -p "Enter Remote Server UserName:" REMOTE_USER
+read -p "Enter Remote Server IPAddress:" REMOTE_IP
+read -p "Enter Remote Server DirBase:" REMOTE_DIR_BASE
+read -p "Enter Remote Local DirBase:" LOCAL_DIR_BASE
 
-echo "rsync -vzrtpl --progress --stats -e ssh ${REMOTE_USER}@${REMOTE_IP}:${REMOTE_DIR_BASE}/$1 $2"
-rsync -vzrtpl --progress --stats -e ssh ${REMOTE_USER}@${REMOTE_IP}:${REMOTE_DIR_BASE}/$1 $2
+echo "rsync -vzrtpl --progress --stats -e ssh ${REMOTE_USER}@${REMOTE_IP}:$REMOTE_DIR_BASE $LOCAL_DIR_BASE"
+rsync -vzrtpl --progress --stats -e ssh ${REMOTE_USER}@${REMOTE_IP}:$REMOTE_DIR_BASE $LOCAL_DIR_BASE
